b6111f2bfa76cc5cd74ed666f73e278be58cc76d
Karl Hobley <karlhobley10@gmail.com>
2016-11-15 22:04:32 +0000

    Restructured palette module

---
+++ src/palette.rs
@@ -0,0 +1,3 @@
+pub const BARREN_PALETTE: &'static [u8] = include_bytes!("palettes/barren.pal");
+pub const JUNGLE_PALETTE: &'static [u8] = include_bytes!("palettes/jungle.pal");
+pub const SNOW_PALETTE: &'static [u8] = include_bytes!("palettes/snow.pal");
